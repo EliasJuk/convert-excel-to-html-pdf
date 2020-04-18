5ce9ad09c2c98a614278d0be29d60f735fe368c5
Elias Juk <eliasp.juk@hotmail.com>
2020-04-18 11:25:46 -0300

    :dizzy: read and write to files

--- Index.js
+++ Index.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
 
-fs.readFile("./test.txt",{encoding: 'utf-8'},(error, dados) => {
-    if(error){
-        console.log('Ocorreu um erro durante a leitura do arquivo!')
-    }else{
-        console.log(dados)
-    }
-})
+function ler(){
+    fs.readFile("./test.txt",{encoding: 'utf-8'},(error, dados) => {
+        if(error){
+            console.log('Ocorreu um erro durante a leitura do arquivo!')
+        }else{
+            console.log(dados)
+        }
+    })
+}
+
+function escrever(){
+    fs.writeFile("./output/test.txt","Hello World", (err) => {
+        console.log("Erro durante o salvamento...")
+    })
+}
+
+escrever()
