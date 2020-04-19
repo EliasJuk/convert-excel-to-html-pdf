4565fed22fdb14a7c3ce824b62a04a6220f9e6ca
Elias Juk <eliasp.juk@hotmail.com>
2020-04-19 11:03:40 -0300

    :memo: Subscrever arquivos

--- Index.js
+++ Index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 
+//Faz a leitura do arquivo e printa no console
 function ler(){
     fs.readFile("./input/test.txt",{encoding: 'utf-8'},(error, dados) => {
         if(error){
@@ -10,12 +11,14 @@ function ler(){
     })
 }
 
+//Cria ou Sobscreve um arquivo e salva na output
 function escrever(){
     fs.writeFile("./output/dados.txt","Hello World", (err) => {
         console.log("Erro durante o salvamento...")
     })
 }
 
+//Faz a leitura do arquivo usuario na pasta input e salva na output
 function jsonfile(){
     fs.readFile("./input/usuario.json",{encoding: 'utf-8'},(err, dados) => {
         if(err){
@@ -27,7 +30,7 @@ function jsonfile(){
             conteudo.nome="Elias"
             console.log(conteudo)
 
-            fs.writeFile("./input/usuario.json", JSON.stringify(conteudo), (err) => {
+            fs.writeFile("./output/usuario.json", JSON.stringify(conteudo), (err) => {
                 if(err){
                     console.log("Um erro ocorreu durante a escrita")
                 }
@@ -36,4 +39,30 @@ function jsonfile(){
     })
 }
 
-jsonfile()
+//Faz a leitura do arquivo usuario na pasta input e salva na output
+function modificarUsuario(nome, curso, categoria){
+    fs.readFile("./input/usuario.json",{encoding: 'utf-8'},(err, dados) => {
+        if(err){
+            console.log("Ocorreu um erro durante a leitura!")
+        }else{
+            var conteudo = JSON.parse(dados)
+
+            conteudo.nome= nome
+            conteudo.curso= curso
+            conteudo.categoria= categoria
+            
+
+            fs.writeFile("./output/usuario.json", JSON.stringify(conteudo), (err) => {
+                if(err){
+                    console.log("Um erro ocorreu durante a escrita")
+                }
+            })
+        }
+    })
+}
+
+
+//ler()
+//escrever()
+//jsonfile()
+modificarUsuario("Amanda", "Master PHP", "PHP")
