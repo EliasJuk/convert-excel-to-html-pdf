00fb86c234150ebf430d27d8be04bc3421098895
Elias Juk <eliasp.juk@hotmail.com>
2020-04-21 15:56:53 -0300

    :file_folder: Reorganizar pastas

---
+++ app/app.js
@@ -0,0 +1,30 @@
+var Reader = require("./Reader")
+var Writer = require('./Writer')
+var Processor = require("./Processor")
+var Table = require("./Table")
+var HtmlParser = require('./HtmlParser')
+
+var leitor = new Reader()
+var escritor = new Writer()
+
+async function main(){
+  var dados = await leitor.Read('../input/users.csv')    
+  var dadosProcessados = Processor.Process(dados)
+  //console.log(dadosProcessados)
+
+  var usuarios = new Table(dadosProcessados)
+  //console.log(usuarios.header)
+  //console.log(usuarios.rows)
+
+  //CONTAGEM DE COLUNAS E LINHAS
+  //  console.log(usuarios.RowCount)
+  //  console.log(usuarios.ColumnCount)
+
+  var html = await HtmlParser.Parse(usuarios)
+  //console.log(html)
+
+  escritor.Write(`../output/${Date.now()}.html`,html)
+
+}
+
+main()
